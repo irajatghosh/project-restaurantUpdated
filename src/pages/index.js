2f01c4eeaa7537db3dfde4fe11d08775ea13ef76
Rajat Ghosh <rajatghosh52@gmail.com>
2019-04-06 20:05:17 +0200

    added menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import { HomeHeader, Banner, BannerButton } from "../utils"
 import img from "../images/bcg/home.jpg"
 import QuickInfo from "../components/HomePageComponents/Quickinfo"
 import Gallary from "../components/HomePageComponents/Gallery-1"
+import Menu from "../components/HomePageComponents/Menu"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
@@ -17,6 +18,7 @@ const IndexPage = () => (
     </HomeHeader>
     <QuickInfo />
     <Gallary />
+    <Menu />
   </Layout>
 )
 
